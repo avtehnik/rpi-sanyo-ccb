6cd5594b5cb50851654d4b2900ea0d69ee454538
Pitvalo Vitaliy <vitaliy@ekreative.com>
2013-03-11 14:12:12 +0200

    remove client

--- make.sh
+++ make.sh
@@ -1,7 +1,3 @@
-gcc -pedantic -Wall -g -O2   -c -o client.o client.c
-
-gcc -o udp_client client.o
-
 gcc -pedantic -Wall -g -O2   -c -o demonize.o demonize.c
 
 gcc -pedantic -Wall -g -O2   -c -o server.o server.c 
