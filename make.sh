5d603fdf39affdee40eebc113a5741b2803ccc4d
Vitaliy Pitvalo <av.tehnik@gmail.com>
2013-03-14 21:58:20 +0000

    clean code

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
-gcc -pedantic -Wall -g -O2   -c -o demonize.o demonize.c
 gcc -pedantic -Wall -g -O2   -c -o fm_control.o fm_control.c 
-gcc -o fm_control fm_control.o demonize.o -lrt -lbcm2835
+gcc -o fm_control fm_control.o -lrt -lbcm2835
